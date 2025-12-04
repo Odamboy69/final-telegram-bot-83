1a41ef03662ccbf99bb638a1b7cc646f36ff009f
salom <7alimbayevv@gmail.com>
2025-12-04 15:34:42 +0500

    chat idsini oladi

--- src/bot/bot.js
+++ src/bot/bot.js
@@ -1,1 +1,15 @@
+import TelegramBot from "node-telegram-bot-api";
+import { config } from "dotenv";
+config()
+
+const bot = new TelegramBot(process.env.BOT_TOKEN,{polling:true})
+
+bot.on("message",(msg) =>{
+    const chatId = msg.chat.id
+    const firsname = msg.chat.first_name
+    const text = msg.text
+    
+ bot.sendMessage(chatId,`Assalomu alaykum,${firsname}`)
+ bot.sendMessage(chatId,`${text}`)
+})
 console.log("bot ishga tushdi");
